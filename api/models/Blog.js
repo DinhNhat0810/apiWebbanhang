cf77c8fa64cbc181d831990913ba0e3ea357c177
DinhNhat0810 <dinhnhat0810@gmail.com>
2022-06-17 11:01:23 +0700

    ok

--- api/models/Blog.js
+++ api/models/Blog.js
@@ -5,7 +5,7 @@ const BlogSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     content: { type: String, required: true },
-    img: { type: String },
+    img: { type: String, default :"https://thinkzone.vn/uploads/2022_01/blogging-1641375905.jpg" },
 
   },
   { timestamps: true }
